1d668af24595dff1a8dd8bf39b3597893d162830
Abe Reyes <abe.raise@gmail.com>
2024-02-10 15:45:21 -0500

    Add fields and modify schema in ProductModel.js

--- server/src/models/ProductModel.js
+++ server/src/models/ProductModel.js
@@ -1,13 +1,21 @@
-// /Users/abiezerreyes/Projects/JewelryWebsite2/server/src/models/ProductModel.js
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: String, // This is an optional field
-  // You can add more fields based on your requirements
+  handle: { type: String, required: true, unique: true }, // Assuming 'Handle' is a unique identifier
+  title: { type: String, required: true },
+  bodyHtml: String, // 'Body (HTML)' field from the CSV
+  vendor: String,
+  type: String,
+  variantSKU: String, // 'Variant SKU'
+  variantPrice: Number, // 'Variant Price'
+  imageSrc: [String], // To accommodate multiple image URLs, use an array of strings
+  imagePosition: [Number], // To store the order of images, use an array of numbers
+  // Add any other fields you deem necessary
+  quantity: { type: Number, default: 1 }, // Set default quantity to 1
 });
 
 const Product = mongoose.model("Product", productSchema);
 
 module.exports = Product;
+
+// /Users/abiezerreyes/Projects/JewelryWebsite2/server/src/models/ProductModel.js
