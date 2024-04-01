e36035e9d23aca648d2f9776f30c94c26e55d14f
Abe Reyes <abe.raise@gmail.com>
2024-04-01 14:35:06 -0400

    Add FormInput component to client/src/components/form

---
+++ client/src/components/form/FormInput.vue
@@ -0,0 +1,39 @@
+<!-- >Users/abiezerreyes/Projects/JewelryWebsite2/client/src/components/form/FormInput.vue -->
+
+<template>
+  <b-form-group
+    :label="label"
+    :label-for="labelFor"
+  >
+    <b-form-input
+      :id="labelFor"
+      v-model="modelValue"
+      :placeholder="placeholder"
+      :required="required"
+      :type="type"
+    ></b-form-input>
+  </b-form-group>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    labelFor: String,
+    placeholder: String,
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String,
+      default: 'text',  // Can be 'text', 'email', 'password', etc.
+    },
+  },
+  emits: ['update:modelValue'],
+};
+</script>
