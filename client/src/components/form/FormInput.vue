ab669f8b52a257da79e39a81a0ac2176400332ad
Abe Reyes <abe.raise@gmail.com>
2024-04-01 21:36:15 -0400

    Refactor FormInput component to improve label and input layout

--- client/src/components/form/FormInput.vue
+++ client/src/components/form/FormInput.vue
@@ -4,14 +4,25 @@
   <b-form-group
     :label="label"
     :label-for="labelFor"
+    :label-cols-sm="labelColsSm"
+    :label-align-sm="labelAlignSm"
+    :label-size="labelSize"
   >
-    <b-form-input
-      :id="labelFor"
-      v-model="modelValue"
-      :placeholder="placeholder"
-      :required="required"
-      :type="type"
-    ></b-form-input>
+    <b-col :sm="contentColsSm">
+      <b-form-input
+        :id="labelFor"
+        :value="inputValue"  
+        @input="updateValue"
+        :placeholder="placeholder"
+        :required="required"
+        :type="type"
+        :state="validState"
+        :disabled="disabled"
+      ></b-form-input>
+      <b-form-invalid-feedback v-if="feedback">
+        {{ feedback }}
+      </b-form-invalid-feedback>
+    </b-col>
   </b-form-group>
 </template>
 
@@ -20,20 +31,51 @@ export default {
   props: {
     label: String,
     labelFor: String,
-    placeholder: String,
     modelValue: {
       type: [String, Number],
-      default: '',
+      default: "",
     },
-    required: {
-      type: Boolean,
-      default: false,
-    },
+    placeholder: String,
+    required: Boolean,
     type: {
       type: String,
-      default: 'text',  // Can be 'text', 'email', 'password', etc.
+      default: "text",
+    },
+    labelColsSm: {
+      type: [String, Number],
+      default: 3,
+    },
+    contentColsSm: {
+      type: [String, Number],
+      default: 9,
+    },
+    labelAlignSm: {
+      type: String,
+      default: "left",
+    },
+    labelSize: String,
+    validState: {
+      type: Boolean,
+      default: null,
+    },
+    feedback: String,
+    disabled: Boolean,
+  },
+  computed: {
+    inputValue: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value);
+      },
     },
   },
-  emits: ['update:modelValue'],
+  methods: {
+    updateValue(value) {
+      this.$emit('update:modelValue', value);
+    },
+  },
+  emits: ["update:modelValue"],
 };
 </script>
