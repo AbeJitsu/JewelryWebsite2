7176887127ce7c3ca52b84aa3a3a511bbb2e575c
Abe Reyes <abe.raise@gmail.com>
2024-04-27 21:00:11 -0400

    Refactor session configuration in session.js and middleware ordering in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
 require("dotenv").config();
+
 const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
@@ -21,14 +22,13 @@ const limiter = rateLimit({
   message: "Too many requests, please try again later.",
 });
 
-
 const port = process.env.PORT || 3000;
 
 app.use(limiter);
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
