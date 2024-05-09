d1c65e0bc3453b33ca180bf6b40196e7ce514795
Abe Reyes <abe.raise@gmail.com>
2024-05-08 20:59:16 -0400

    Refactor vue.config.js to update alias path for source directory

--- client/vue.config.js
+++ client/vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
     resolve: {
       // Define aliases for directories
       alias: {
-        "@": path.resolve(__dirname, "src"),
+        "@": path.resolve(__dirname, "src/"),
       },
     },
   },
