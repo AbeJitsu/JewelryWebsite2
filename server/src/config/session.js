27a8e57f70bd33774ddef953a6ce181f4c021ae9
Abe Reyes <abe.raise@gmail.com>
2024-05-10 21:26:13 -0400

    Brought over session.js from machine 1 and resolved the session error. Now dealing with an issue where products aren't loading

---
+++ server/src/config/session.js
@@ -0,0 +1,28 @@
+// /Users/abiezerreyes/Documents/JewelryWebsite2/server/src/config/session.js
+
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+const createSessionConfig = () => {
+  const config = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false, // To ensure carts are created for unauthenticated users
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+      collectionName: "sessions",
+      ttl: 24 * 60 * 60,
+      autoRemove: "native",
+    }),
+    cookie: {
+      secure: process.env.NODE_ENV === "production", // False if not in production
+      httpOnly: true,
+      sameSite: "lax",
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    },
+  };
+  console.log("Session Config:", config);
+  return config;
+};
+
+module.exports = createSessionConfig;
